5518a9d0f827aedb4f328d0b80b24434878204b7
Felipe Ladislau <felipe.s.ladislau@gmail.com>
2020-01-10 10:52:37 -0300

    Primeiro commit

---
+++ settings.php
@@ -0,0 +1,7 @@
+<?php
+	$settings = array(
+		"clientID" => "SEU-ID-NO-APP-DO-INSTAGRAM",
+		"clientSecret" => "SEU-SEGREDO-NO-APP-DO-INSTAGRAM",
+		"redirectURI" => "http://seudominio.com.br/sandbox/in_login/callback.php"
+	);
+?>
